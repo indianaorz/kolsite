47e16f6de97d6fdc6852af09fa652ce114ea5135
indianaorz <indianaorz@gmail.com>
2020-01-17 21:40:20 -0500

    added app bar

--- control.js
+++ control.js
@@ -4,15 +4,13 @@ var content;
 document.addEventListener("DOMContentLoaded", function(event) { 
     elements = document.getElementsByClassName("hero");
     content = document.getElementsByClassName("content")[0];
-    setTimeout(function(){
-    content.style.top = elements[0].offsetHeight+ "px";
-    }, 200);
+    setTimeout(OnScroll, 200);
 });  
 
 
 function OnScroll(){
     for(var i = 0; i < elements.length; i++){
-        elements[i].style.top = -(document.documentElement.scrollTop ) * (Math.pow(i,2)  / Math.pow(elements.length,2)) + "px";
+        elements[i].style.top = -(document.documentElement.scrollTop - 72 ) * (Math.pow(i,2)  / Math.pow(elements.length,2)) + "px";
     }
-    content.style.top = elements[0].offsetHeight -(document.documentElement.scrollTop )+ "px";
+    content.style.top = elements[0].offsetHeight -(document.documentElement.scrollTop - 72)+ "px";
 }
