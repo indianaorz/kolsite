d7cb95e530b0d7f5e5b62a74fe8f8b0ed9007f19
indianaorz <indianaorz@gmail.com>
2020-01-17 21:24:34 -0500

    fixed image

--- control.js
+++ control.js
@@ -4,13 +4,15 @@ var content;
 document.addEventListener("DOMContentLoaded", function(event) { 
     elements = document.getElementsByClassName("hero");
     content = document.getElementsByClassName("content")[0];
+    setTimeout(function(){
     content.style.top = elements[0].offsetHeight+ "px";
+    }, 200);
 });  
 
 
 function OnScroll(){
     for(var i = 0; i < elements.length; i++){
-        elements[i].style.top = -(document.documentElement.scrollTop ) * (i / elements.length) + "px";
+        elements[i].style.top = -(document.documentElement.scrollTop ) * (Math.pow(i,2)  / Math.pow(elements.length,2)) + "px";
     }
     content.style.top = elements[0].offsetHeight -(document.documentElement.scrollTop )+ "px";
 }
