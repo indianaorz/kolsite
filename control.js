b52ee79113a8f98c4c2dffd4f8efa49c526b691f
indianaorz <indianaorz@gmail.com>
2020-01-18 13:26:34 -0500

    made brighter

--- control.js
+++ control.js
@@ -1,9 +1,15 @@
 var elements;
 var content;
+var fogs;
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     elements = document.getElementsByClassName("hero");
     content = document.getElementsByClassName("content")[0];
+    fogs = document.getElementsByClassName("fog");
+    
+    for(var i = 0; i < fogs.length; i++){
+        fogs[i].style.zIndex = i;
+    }
     setTimeout(OnScroll, 200);
 });  
 
@@ -11,6 +17,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function OnScroll(){
     for(var i = 0; i < elements.length; i++){
         elements[i].style.top = -(document.documentElement.scrollTop - 72 ) * (Math.pow(i,2)  / Math.pow(elements.length,2)) + "px";
+        elements[i].style.zIndex = i;
     }
     content.style.top = elements[0].offsetHeight -(document.documentElement.scrollTop - 72)+ "px";
 }
