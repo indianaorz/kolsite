210515be42cbcdf0089eb47c5925378b0cff3437
Lee <lee.e.orzol@gmail.com>
2021-01-28 14:01:43 -0500

    added battling

--- ConversingOfSouls/control.js
+++ ConversingOfSouls/control.js
@@ -29,6 +29,11 @@ function ResetGame() {
 }
 
 function RefreshGameOutput() {
+
+    GetDetails();
+    GetAllCharacters();
+    GetAttacks();
+
     var url = document.getElementById("server").value + "/Output";
 
     $.ajax({
@@ -108,6 +113,10 @@ function ChangeCharacter() {
         success: function (data) {
             AssignCharacterData("character", data);
 
+            total = data.essence;
+            var element = document.getElementById("character-card");
+            element.style.boxShadow = "0px 0px 20px " + total + "px  hsl(0,0%, " + (((total) * 100)).toString() + "%)";
+
             //After changing the character, get the trait focus data relative to the player
             GetTraitFocusData();
         },
@@ -148,6 +157,11 @@ function ChangePlayer() {
             AssignCharacterData("player", data[0]);
             AssignCharacterData("player", data[0]);
 
+            total = data[0].essence;
+            var element = document.getElementById("player-card");
+            element.style.boxShadow = "0px 0px 20px " + total + "px  hsl(0,0%, " + (((total) * 100)).toString() + "%)";
+
+
             //After changing the player, get the trait focus data relative to them
             GetTraitFocusData();
         },
@@ -164,10 +178,11 @@ function AssignCharacterData(idPrefix, data) {
 
     //Assign Name
     document.getElementById(idPrefix + "-name").innerHTML = data.name;
+    document.getElementById(idPrefix + "-health").innerHTML = data.health;
 
     //Assign traits
     AssignTraits(idPrefix, "traits", data.traits);
-    AssignTraits(idPrefix, "subtexts", data.subtexts);
+    AssignTraits(idPrefix, "subtexts", data.personalDetail);
 }
 
 function AssignTraits(idPrefix, idSuffix, data) {
@@ -179,14 +194,142 @@ function AssignTraits(idPrefix, idSuffix, data) {
     element.innerHTML = "";
     data.forEach(function (trait) {
         element.innerHTML +=
-            "<div class = 'trait' onclick=SelectTarget('" + trait + "')>" + trait + "</div>"
+            "<div class = 'trait' onclick=SelectTarget('" + trait.toString().replaceAll(" ", "~") + "','" + idPrefix + "')>" + trait.toString() + "</div>"
     });
 }
 
 
-function SelectTarget(detail) {
-    if (document.getElementById("BaseAction" + activeActionIndex).value != "Rest"
-        && detail != undefined) {
+function GetDetails() {
+
+    var url = document.getElementById("server").value + "/Details";
+
+    $.ajax({
+        type: "GET",
+        url: url,
+        crossDomain: true,
+        success: function (data) {
+
+            AssignTraits("detail", "traits", data);
+
+            GetTraitFocusData();
+        },
+        error: function (data) {
+            console.log(JSON.stringify(data));
+        },
+        dataType: "json",
+        contentType: "application/json; charset=utf-8"
+    });
+
+}
+
+function AddDetail() {
+
+    var url = document.getElementById("server").value + "/Details";
+    var detail = document.getElementById("detailInfo").value;
+
+    var body = {
+        "detail": detail
+    };
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: JSON.stringify(body),
+        crossDomain: true,
+        success: function (data) {
+            AssignTraits("detail", "traits", data);
+            GetTraitFocusData();
+        },
+        error: function (data) {
+            console.log(JSON.stringify(data));
+        },
+        dataType: "json",
+        contentType: "application/json; charset=utf-8"
+    });
+
+}
+
+
+function GetAttacks() {
+
+    var url = document.getElementById("server").value + "/Attacks";
+
+    $.ajax({
+        type: "GET",
+        url: url,
+        crossDomain: true,
+        success: function (data) {
+
+            AssignAttacks("attack", "traits", data);
+
+            GetTraitFocusData();
+
+            
+
+        },
+        error: function (data) {
+            console.log(JSON.stringify(data));
+        },
+        dataType: "json",
+        contentType: "application/json; charset=utf-8"
+    });
+
+}
+
+function AssignAttacks(idPrefix, idSuffix, data) {
+
+    if (data == null) {
+        return;
+    }
+    var element = document.getElementById(idPrefix + "-" + idSuffix);
+    element.innerHTML = "";
+    data.forEach(function (attack) {
+        element.innerHTML +=
+            "<div class = 'trait' onclick=SelectTarget('Attack~on~" + attack.trait.toString().replaceAll(" ", "~") + "','" + idPrefix + "')"
+            + " style = 'box-shadow: 0px 0px 20px " + (attack.essence * 5) + "px hsl(0,50%,50%)'" +
+            "> Attack on " + attack.trait.toString() + "</div>"
+    });
+}
+
+
+function AddPersonalDetail() {
+
+    var url = document.getElementById("server").value + "/Character/Details";
+    var detail = document.getElementById("personalDetailInfo").value;
+    var id = document.getElementById("PlayerSelect").value;
+
+    var body = {
+        "characterId": id,
+        "detail": detail
+    };
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: JSON.stringify(body),
+        crossDomain: true,
+        success: function (data) {
+            AssignTraits("player", "subtexts", data);
+        },
+        error: function (data) {
+            console.log(JSON.stringify(data));
+        },
+        dataType: "json",
+        contentType: "application/json; charset=utf-8"
+    });
+
+}
+
+
+function SelectTarget(detail, from) {
+
+    detail = detail.replaceAll("~", " ");
+    var actionSelected = document.getElementById("BaseAction" + activeActionIndex).value;
+    if (actionSelected != "Rest"
+        && detail != undefined
+        && actionSelected != "Control"
+        || actionSelected == "Control"
+        && from == "player") {
         document.getElementById("target" + activeActionIndex).innerHTML = detail;
         SetTargetType("Trait");
     }
@@ -194,6 +337,7 @@ function SelectTarget(detail) {
         document.getElementById("target" + activeActionIndex).innerHTML = "";
         SetTargetType("");
     }
+
 }
 
 
@@ -224,10 +368,13 @@ function EndRound() {
 }
 
 var activeActionIndex = 0;
+
 function SetActiveActionIndex(index) {
     activeActionIndex = index;
 
-    if (document.getElementById("BaseAction" + activeActionIndex).value == "Rest") {
+    var actionSelected = document.getElementById("BaseAction" + activeActionIndex).value;
+    if (actionSelected == "Rest"
+        || actionSelected == "Control") {
         document.getElementById("target" + activeActionIndex).innerHTML = "";
     }
 }
@@ -242,6 +389,7 @@ function SendAction() {
     var baseAction3 = document.getElementById("BaseAction3").value;
     var target3 = document.getElementById("target3").innerHTML;
     var dialogue = document.getElementById("dialogue").value;
+    var dialogue2 = document.getElementById("dialogue2").value;
     var url = document.getElementById("server").value + "/Round/Action";
 
     var body = {
@@ -260,7 +408,7 @@ function SendAction() {
                 "target": target3,
             }
         ],
-        "dialogue": dialogue
+        "dialogue": dialogue + "<br>" + dialogue2
     };
 
     $.ajax({
@@ -284,6 +432,7 @@ function CompleteSubmit(data) {
 
     //Clear the dialogue to avoid double submit
     document.getElementById("dialogue").value = "";
+    document.getElementById("dialogue2").value = "";
 
     //Reset active action index
     SetActiveActionIndex(1);
@@ -301,6 +450,8 @@ function CompleteSubmit(data) {
 
     ChangeCharacter();
     GetTraitFocusData();
+    GetAttacks();
+
 }
 
 
@@ -331,20 +482,24 @@ function ProcessTraitFocusData(data) {
     if (traitElements == undefined) {
         return;
     }
-    data.traitFocusData.forEach(function (traitFocusData) {
-        for (let visibleTraitElement of traitElements) {
-            if (visibleTraitElement.innerHTML == traitFocusData.trait) {
 
-                var yourValue = traitFocusData.yourFocus;
-                var otherValue = traitFocusData.otherFocus;
+
+    data.targetFocusData.forEach(function (targetFocusData) {
+        for (let visibleTraitElement of traitElements) {
+            if (visibleTraitElement.innerHTML == targetFocusData.target
+                && (targetFocusData.yourFocus > 0
+                    || targetFocusData.otherFocus > 0)) {
+
+                var yourValue = targetFocusData.yourFocus;
+                var otherValue = targetFocusData.otherFocus;
 
                 var strength = Math.abs(yourValue - otherValue);
                 var difference = yourValue - otherValue;
                 var total = yourValue + otherValue;
 
 
-                visibleTraitElement.style.boxShadow = "0px 0px 20px " + strength + "px  hsl(0,0%, " + (50 + ((difference/total) * 50)).toString() + "%)";
-                visibleTraitElement.style.zIndex = difference + total;
+                visibleTraitElement.style.boxShadow = "0px 0px 20px " + total + "px  hsl(0,0%, " + (50 + ((difference / total) * 50)).toString() + "%)";
+                visibleTraitElement.style.zIndex = total;
 
 
             }
